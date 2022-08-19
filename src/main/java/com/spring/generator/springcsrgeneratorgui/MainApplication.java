f230f316ee7480c2a7708f12c8b3e71c12d81ad5
DRACOX2500 <alexis.forestier2001@gmail.com>
2022-08-19 18:49:11 +0200

    [Import] delete some imports

--- src/main/java/com/spring/generator/springcsrgeneratorgui/MainApplication.java
+++ src/main/java/com/spring/generator/springcsrgeneratorgui/MainApplication.java
@@ -1,11 +1,5 @@
 package com.spring.generator.springcsrgeneratorgui;
 
-import com.spring.generator.springcsrgeneratorgui.fx.StageFX;
-import javafx.stage.Stage;
-import javafx.application.Application;
-
-import java.io.IOException;
-
 public class MainApplication {
 
     public static void main(String[] args) {
